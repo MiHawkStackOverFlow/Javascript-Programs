9340eb2d32c8e842d9c9df9f40dd2286491a5fe4
Abhishek Sharma <abhivsharma8@gmail.com>
2019-11-12 22:15:11 +0530

    one more comment

--- my programs/design patterns/facade.js
+++ my programs/design patterns/facade.js
@@ -1,3 +1,10 @@
+/**
+ * may conceal a very different reality. This was the inspiration for the name behind the pattern we’re going to review:
+ * the Facade pattern. This pattern provides a convenient higher-level interface to a larger body of code,
+ * hiding its true underlying complexity.
+ */
+
+
 /* here we’re using a Facade to simplify an interface for listening to events across browsers. 
 We do this by creating a common method that can be used in one’s code which does the task of 
 checking for the existence of features so that it can provide a safe and cross-browser compatible solution. */
